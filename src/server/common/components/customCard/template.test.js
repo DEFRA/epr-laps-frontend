3748a2febc81081bd2c2f6a8ddb7313a8fb4af66
juhiupreti-acc <juhi.upreti@accenture.com>
2025-09-05 09:14:36 +0100

    Fix unit tests

---
+++ src/server/common/components/customCard/template.test.js
@@ -0,0 +1,24 @@
+import { renderComponent } from '../../test-helpers/component-helpers.js'
+
+describe('Custom Card Component', () => {
+  let $customCard
+
+  describe('With caption', () => {
+    beforeEach(() => {
+      $customCard = renderComponent('customCard', {
+        title: 'Services',
+        caption: 'A page showing available services'
+      })
+    })
+
+    test('Should render app customCard component', () => {
+      expect($customCard('[data-testid="app-custom-card"]')).toHaveLength(1)
+    })
+
+    test('Should contain expected customCard', () => {
+      expect(
+        $customCard('[data-testid="app-customCard-title"]').text().trim()
+      ).toBe('Services')
+    })
+  })
+})
