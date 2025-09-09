3de9c8ce815bcbb9174b649f636e79dc47a6e82f
Juely <juely.kaikade@accenture.con>
2025-09-09 13:46:24 +0100

    Controller.js, page.njk modified

--- src/server/home/controller.js
+++ src/server/home/controller.js
@@ -8,14 +8,12 @@ export const homeController = {
 
     return h.view('home/index', {
       pageTitle: 'Home',
-      // heading: translations['local-authority'] || 'local-authority',
       currentLang,
       translations,
       breadcrumbs: [
         {
-          text:
-            translations['local-authority'] ||
-            'Local Authority Payments (LAPs) home',
+           text:
+            translations['local-authority'],
           href: '/'
         }
       ]
