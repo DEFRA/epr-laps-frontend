ae068350586f40b9755d91a15fb1ca44b892cb93
Juely <juely.kaikade@accenture.con>
2025-10-01 15:06:48 +0100

    updated the sign-out controller.js and unit test

--- src/server/sign-out/controller.js
+++ src/server/sign-out/controller.js
@@ -1,5 +1,12 @@
+import { removeUserSession } from '../common/helpers/auth/utils.js'
+
 export const signOutController = {
-  handler: (_request, h) => {
+  handler: async (request, h) => {
+    // If user session exists, remove it
+    if (request?.state?.userSession) {
+      await removeUserSession(request)
+    }
+
     return h.view('sign-out/index.njk', {
       pageTitle: 'Sign out',
       heading: 'Glamshire County Council'
