3a2e57dbcb3dd253f5080f36f7f56b0b300f9b9f
juhiupreti-acc <juhi.upreti@accenture.com>
2025-11-04 12:32:49 +0000

    Fix test

--- src/server/common/helpers/utils.test.js
+++ src/server/common/helpers/utils.test.js
@@ -1,4 +1,28 @@
-import { formatDuration } from './utils.js'
+import { mapPermissions, formatDuration } from './utils.js'
+
+describe('#mapPermissions', () => {
+  const permissionConfig = {
+    viewFullBankDetails: ['CEO'],
+    updateBankDetails: ['HOF']
+  }
+  test('should return true when role is found in permission action', () => {
+    const userPermission = mapPermissions(
+      permissionConfig,
+      'Chief Executive Officer'
+    )
+    expect(userPermission).toEqual({
+      viewFullBankDetails: true,
+      updateBankDetails: false
+    })
+  })
+  test('should return appropriate permission when role is found', () => {
+    const userPermission = mapPermissions(permissionConfig, 'Normal User')
+    expect(userPermission).toEqual({
+      viewFullBankDetails: false,
+      updateBankDetails: false
+    })
+  })
+})
 describe('formatDuration', () => {
   it('returns seconds when duration is less than a minute', () => {
     const result = formatDuration(5000) // 5 seconds
