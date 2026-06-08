3043764c85cae523e5c9da326c0efe11b23dde32
juhiupreti-acc <juhi.upreti@accenture.com>
2026-06-08 12:59:11 +0100

    LAPS accessibility controller

--- src/server/accessibility-statement/controller.js
+++ src/server/accessibility-statement/controller.js
@@ -1,21 +1,15 @@
 /**
- * A GDS styled example get help controller
+ * A GDS styled example accessibility statement controller
  */
-
-import { config } from '../../config/config.js'
 
 export const accessibilityController = {
   handler: (request, h) => {
     const { currentLang, translations } = request.app
-    const link = config.get('getHelpUrl')
-    const customerEmail = config.get('customerServiceEmail')
 
     return h.view('accessibility-statement/index.njk', {
-      pageTitle: 'Get Help',
+      pageTitle: 'Accessibility Statement',
       currentLang,
       translations,
-      getHelpUrl: link,
-      customerServiceEmail: customerEmail,
       breadcrumbs: [
         {
           text: translations['laps-home'],
